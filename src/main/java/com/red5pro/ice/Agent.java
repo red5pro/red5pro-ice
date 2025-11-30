43a322bdc0315d90cde7c7955c7b0b190de938f8
Paul Gregoire <mondain@gmail.com>
2025-11-29 16:18:04 -0800

    Update version to 1.2.5. Support added for ice-lite

--- src/main/java/com/red5pro/ice/Agent.java
+++ src/main/java/com/red5pro/ice/Agent.java
@@ -75,7 +75,7 @@ public class Agent {
     /**
      * The version of the library.
      */
-    private final static String VERSION = "1.2.4";
+    private final static String VERSION = "1.2.5";
 
     /**
      * Secure random for shared use.
